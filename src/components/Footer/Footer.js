16a9498ca22b16a0533c6de0c47421e014e78f43
mac <h.makusota@outlook.com>
2021-10-06 12:27:16 +0100

    footer completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,12 @@ import './Footer.css'
 import imgFoot from './../../images/logo.svg'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faTwitter } from '@fortawesome/free-brand-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 // import { faTwitter, faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brand-svg-icons'
 
+import ReactDOM from 'react-dom'
 
+library.add(fab)
 
 
 
@@ -20,29 +22,39 @@ function Footer() {
             <div className="top-footer">
                 <div className="top-main">
                     <img src={imgFoot} alt="logo"/>
-                    <p>Looking for job and like what you see? check out our <NavLink to="/">open positions!</NavLink> </p>
+                    <p>Looking for job and like what you see? check out our <NavLink  className="post" to="/">open positions!</NavLink> </p>
                 </div>
             </div>
             <hr/>
 
             {/* bottom footer */}
             <div className="bottom-footer">
-                <p>©2020  is a registered trademark of WebToGo GmbH I Data Privacy I Imprint</p>
+                <p>©2020  is a registered trademark of WebToGo GmbH I <NavLink className="data" to="/"> Data Privacy </NavLink>   I <NavLink className="data" to="/">Imprint</NavLink></p>
                 <ul class="social">
                     <li>
                         <NavLink target="_blank" to="https://www.facebook.com/myhandycheck">
-                            {/* <FontAwesomeIcon icon={faTwitter}/> */}
-                            {/* <FontAwesomeIcon icon={faFacebook}/> */}
-                            {/* <FontAwesomeIcon icon={faInstagram}/> */}
-                            {/* <FontAwesomeIcon icon={faLinkedin}/> */}
-                            <FontAwesomeIcon icon={['fab', 'twitter']} />
-
+                            <FontAwesomeIcon icon={['fab', 'facebook']} />
                         </NavLink>
                     </li>
-                    {/* <li class="facebook"><NavLink target="_blank" to="https://www.facebook.com/myhandycheck" title="Facebook"><FontAwesomeIcon class="icon-facebook"></FontAwesomeIcon></NavLink></li>
-                    <li class="twitter"><a target="_blank" href="https://twitter.com/myhandycheck" title="Twitter"><i class="icon-twitter"></i></a></li>
-                    <li class="linkedin"><a target="_blank" href="https://www.linkedin.com/company/myhandycheck/" title="LinkedIn"><i class="icon-linkedin"></i></a></li>
-                    <li class="instagram"><a target="_blank" href="https://www.instagram.com/myhandycheck/" title="Instagram"><i class="icon-instagram"></i></a></li> */}
+
+                       <li>
+                        <NavLink target="_blank" to="https://twitter.com/myhandycheck">
+                            <FontAwesomeIcon icon={['fab', 'twitter']} />
+                        </NavLink>
+                    </li>
+
+                       <li>
+                        <NavLink target="_blank" to="https://www.linkedin.com/company/myhandycheck/">
+                            <FontAwesomeIcon icon={['fab', 'linkedin']} />
+                        </NavLink>
+                    </li>
+
+                       <li>
+                        <NavLink target="_blank" to="https://www.instagram.com/myhandycheck/">
+                            <FontAwesomeIcon icon={['fab', 'instagram']} />
+                        </NavLink>
+                    </li>
+                
                 </ul>
             </div>
         </div>
